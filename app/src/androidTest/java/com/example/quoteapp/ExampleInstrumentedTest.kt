57aa91b70fa5cc8e336f47a75e3acc749cd15f1e
Aryan Rathi <aryan2001rathi@gmail.com>
2023-10-11 01:06:09 +0530

    Changes is import file of ExampleInstrumentedTest.kt.

--- app/src/androidTest/java/com/example/quoteapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/quoteapp/ExampleInstrumentedTest.kt
@@ -1,16 +1,19 @@
 package com.example.quoteapp
 
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
+//import androidx.test.ext.junit.runners.AndroidJUnit4
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
+//import org.junit.jupiter.api.Assertions.assertEquals
+//import org.junit.jupiter.api.Test
 
 /**
  * Instrumented test, which will execute on an Android device.
- *
+ * run
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
